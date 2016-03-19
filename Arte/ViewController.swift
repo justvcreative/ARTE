28050101fb39218e33ac04797e4c2f284b626e65
justvcreative <justvcreative@gmail.com>
2016-03-19 00:50:12 -0400

    Got TwitterKit and Social Framework to work with the Twitter API

---
+++ Arte/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Arte
+//
+//  Created by Veronica on 3/18/16.
+//  Copyright © 2016 JustVCreative. All rights reserved.
+//
+
+import UIKit
+import Social
+
+class ViewController:  UIViewController {
+   
+    @IBAction func TweetAction(sender: AnyObject) {
+        if SLComposeViewController.isAvailableForServiceType(SLServiceTypeTwitter) {
+            let tweetController = SLComposeViewController(forServiceType: SLServiceTypeTwitter)
+            tweetController.setInitialText("Check out @Th3Ba1r0n's artwork on #ARTE App")
+            self.presentViewController(tweetController, animated: true, completion: nil)
+        } else {
+            let alert =  UIAlertController(title: "Accounts", message: "Please Log In To Your Twitter Account", preferredStyle: UIAlertControllerStyle.Alert)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+            alert.addAction(UIAlertAction(title: "Settings", style: UIAlertActionStyle.Default, handler: {
+                (UIAlertAction) in
+                let settingsURL = NSURL(string: UIApplicationOpenSettingsURLString)
+                if let url = settingsURL {
+                    UIApplication.sharedApplication().openURL(url)
+                }
+                self.presentViewController(alert, animated: true, completion: nil)
+            }))
+        }
+    }
+}
